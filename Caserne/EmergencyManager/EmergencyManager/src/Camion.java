84d88e57498d27e2ec74829b5257337ff3c41432
PhAr3N <antonin.poulard3@orange.fr>
2021-01-08 17:09:07 +0100

    First comit beginning of the app

---
+++ Caserne/EmergencyManager/EmergencyManager/src/Camion.java
@@ -0,0 +1,11 @@
+public class Camion {
+    private int pouvoirDArret;
+    private Caserne caserne ;
+
+    public Camion(Coordonees coords, int pda)
+    {
+        Coordonees coord = coords;
+        int pouvoirDArret = pda;
+        
+    }
+}
