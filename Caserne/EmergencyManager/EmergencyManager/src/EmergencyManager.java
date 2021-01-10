925619af009a078e3f016ea396d166d77af9d691
PhAr3N <antonin.poulard3@orange.fr>
2021-01-10 12:46:32 +0100

    Commit pour bosser chez moi

--- Caserne/EmergencyManager/EmergencyManager/src/EmergencyManager.java
+++ Caserne/EmergencyManager/EmergencyManager/src/EmergencyManager.java
@@ -1,12 +1,43 @@
+import java.lang.Math;
+
 public class EmergencyManager {
-    public static void main (String[] args){
+
+
+    public static void main(String[] args) {
         System.out.println("Bienvenue dans l'emergency manager");
+        while (true) {
 
-
-
-
-
+        }
 
     }
+
+    public double calculeDistance(Coordonees coordUn, Coordonees coordDeux) {
+        return Math.sqrt(((coordUn.getX() - coordDeux.getX()) * (coordUn.getX() - coordDeux.getX())) + ((coordUn.getY() - coordDeux.getY()) * (coordUn.getY() - coordDeux.getY())));
+    }
+
+    public boolean everyFeuHasIntervention() {
+
+        boolean result = false;
+
+        foreach(capteur in bdd)
+        {
+            if (intensite > 0) {
+                foreach intervention
+                {
+                    while (!result) {
+                        if (intervention.getFeu() == feu) {
+                            result = true;
+                        }
+                    }
+
+                }
+
+            }
+        }
+    }
+
+
 }
+
+
 
