f1859c4012affc845cac7d1c745b687a238bae2a
PhAr3N <antonin.poulard3@orange.fr>
2021-01-12 00:53:26 +0100

    Fix des bêtises du git

--- Caserne/EmergencyManager/EmergencyManager/src/EmergencyManager.java
+++ Caserne/EmergencyManager/EmergencyManager/src/EmergencyManager.java
@@ -14,7 +14,7 @@ public class EmergencyManager {
     public double calculeDistance(Coordonees coordUn, Coordonees coordDeux) {
         return Math.sqrt(((coordUn.getX() - coordDeux.getX()) * (coordUn.getX() - coordDeux.getX())) + ((coordUn.getY() - coordDeux.getY()) * (coordUn.getY() - coordDeux.getY())));
     }
-
+/*
     public boolean everyFeuHasIntervention() {
 
         boolean result = false;
@@ -35,7 +35,7 @@ public class EmergencyManager {
             }
         }
     }
-
+*/
 
 }
 
